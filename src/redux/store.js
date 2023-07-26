a5bc45558dffc0164c873b99b7dcdb9cb2ba6e2b
rbd3 <rabedesanaa@gmail.com>
2023-07-26 16:58:15 +0300

    Fix linters error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import missionSlice from './missions/missionSlice';
-import RocketsSlice from './Rockets/RocketsSlice';
+import missionSlice from './missions/missionsSlice';
+import RocketsSlice from './rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
