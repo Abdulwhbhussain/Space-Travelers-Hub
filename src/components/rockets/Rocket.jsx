b3fe33e8e5810df5ab6b6372dfcdbd16a2391f29
WHB901 <abdulwhbhussain@gmail.com>
2023-07-27 16:45:55 +0500

    rockets

--- src/components/rockets/Rocket.jsx
+++ src/components/rockets/Rocket.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import './Rocket.css';
 import { useDispatch } from 'react-redux';
 import { toggleReserve } from '../../redux/rockets/rocketsSlice';
+
+const StyledRocket = styled.div`
+  display: flex;
+  height: 250px;
+  margin: 20px;
+  .image {
+    width: 350px;
+    margin-right: 20px;
+    img {
+      width: 350px;
+      height: 100%;
+      object-fit: fill;
+    }
+  }
+  button {
+    border-radius: 5px;
+    font-size: 20px;
+    padding: 10px 20px;
+    border: 1px solid #6c757d;
+  }
+`;
 
 function Rocket({ key, rocket }) {
   const dispatch = useDispatch();
@@ -11,19 +34,26 @@ function Rocket({ key, rocket }) {
   }
 
   return (
-    <div key={key}>
-      <div>
-        <img src={rocket.image} alt={rocket.name} />
+    <StyledRocket key={key} id="mobileVersion">
+      <div className="image">
+        <img id="rocket-image" src={rocket.image} alt={rocket.name} />
       </div>
       <div>
         <h2>{rocket.name}</h2>
         <p>
-          {rocket.reserved && <span style={{ color: '#fff', background: '#007bff' }}>Reserved</span>}
+          {rocket.reserved && (
+          <span style={{
+            color: '#fff', background: '#007bff', marginRight: '20px', borderRadius: '5px', padding: '2px 5px',
+          }}
+          >
+            Reserved
+          </span>
+          )}
           {rocket.description}
         </p>
         <button style={rocket.reserved ? { color: '#6c757d', background: '#fff' } : { color: '#fff', background: '#007bff' }} type="button" onClick={() => toggleReserved(rocket.id)}>{ rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket' }</button>
       </div>
-    </div>
+    </StyledRocket>
   );
 }
 
