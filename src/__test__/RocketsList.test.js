414f2ae634e83e51da6e16d78c5e46050d79e67f
rbd3 <rabedesanaa@gmail.com>
2023-07-28 20:36:01 +0300

    Fix linters error

--- src/__test__/RocketsList.test.js
+++ src/__test__/RocketsList.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render,
-    screen,
-    fireEvent
+import {
+  render,
+  screen,
+  fireEvent,
 } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
